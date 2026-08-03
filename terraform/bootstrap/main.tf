aa974e8eef7984f7e1a0bc4169e041039bfc8c53
sky7-devops <gaganbiswas@MacBookAir.lan>
2026-08-04 01:38:20 +0530

    Added terraform state lock

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -30,3 +30,18 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   block_public_policy     = true
   restrict_public_buckets = true
 }
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform State Lock"
+  }
+}
