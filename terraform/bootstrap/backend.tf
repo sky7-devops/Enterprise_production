815b8fe14b1def05b8e8bee5fae901493f456e24
sky7-devops <gaganbiswas@MacBookAir.lan>
2026-08-04 01:41:42 +0530

    Added s3 backend

---
+++ terraform/bootstrap/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "gaganbiswas-terraform-state-ap-south-1"
+    key            = "bootstrap/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
